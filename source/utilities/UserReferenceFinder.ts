34ce954a84c2952decb02912bc0626b5257e6e9a
Hextanium <hextanium.on.discord@gmail.com>
2021-06-20 16:56:31 -0400

    [util] Add a simple, regex based `UserReferenceFinder`

---
+++ source/utilities/UserReferenceFinder.ts
@@ -0,0 +1,40 @@
+import { Client, User } from "discord.js";
+import regexes from "./UserReferenceRegexes";
+
+export class UserReferenceFinder {
+    private _strings: string[];
+    private _client: Client;
+
+    constructor(client : Client, args: string[]) {
+        this._strings = args;
+        this._client = client;
+    }
+
+    findMemberReferences() : User[] {
+        const EMPTY_USER = new User(this._client, {});
+        const users : User[] = [];
+
+        this._strings.forEach(arg => {
+            if (this.returnReferenceIn(arg))
+                users.push(this.returnReferenceIn(arg) || EMPTY_USER);
+        });
+
+        return users;
+    }
+
+    private returnReferenceIn(memberString: string) : User | undefined {
+        if (regexes.USER_TAG.test(memberString)) {
+            return this._client.users.cache.find(user => `${user.username}#${user.tag}` == memberString);
+        }
+
+        if (regexes.USER_MENTION.test(memberString)) {
+            return this._client.users.cache.find(user => [`<@${user.id}>`, `<@!${user.id}>`].includes(memberString));
+        }
+
+        if (regexes.USER_ID.test(memberString)) {
+            return this._client.users.cache.get(memberString);
+        }
+
+        return undefined;
+    }
+}
---
+++ source/utilities/UserReferenceRegexes.ts
@@ -0,0 +1,5 @@
+export default {
+    USER_TAG: /(.|#)+(#)\d{4}$/,
+    USER_MENTION: /<@!?([0-9]{17,19})>$/,
+    USER_ID: /([0-9]{17,19})$/,
+}

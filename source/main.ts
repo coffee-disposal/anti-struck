fd6710187eef2d8e2c098ed22f050706137220e8
Hextanium <hextanium.on.discord@gmail.com>
2021-06-23 20:04:31 -0400

    [fix] remove unnecessary async, make it actually work

--- source/main.ts
+++ source/main.ts
@@ -1,12 +1,12 @@
 import AntiClient from "./utilities/AntiClient";
 import { config as _envConfig } from "dotenv";
 
-( async () => {
-    _envConfig();
-    const client : AntiClient = new AntiClient();
-    
-    //Use CLIENT_TOKEN as the env variable name for the token
-    if ((await client.register() == ''))
-        throw new Error("There was an error while logging in.");
-    console.info(`Logged in as ${client.user?.tag}`);
-})();
+_envConfig();
+
+const client: AntiClient = new AntiClient();
+
+client.login(process.env.TOKEN).catch(err => {
+    throw new Error("There was an error while logging in.\n" + err);
+});
+
+console.info(`Logged in as ${client.user}`);

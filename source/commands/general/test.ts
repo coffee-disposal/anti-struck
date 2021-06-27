fcb1e16c7b6dc3cede53ce51e7980731164339db
Hextanium <hextanium.on.discord@gmail.com>
2021-06-27 13:01:19 -0400

    [test] create command for testing

---
+++ source/commands/general/test.ts
@@ -0,0 +1,8 @@
+import {Command} from "../../structures/Command";
+import {PermissionFlags} from "discord.js";
+
+export class Test extends Command {
+    constructor(name: string, category: string, aliases: string[], description: string, permissions: PermissionFlags[]) {
+        super(name, category, aliases, description, permissions);
+    }
+}

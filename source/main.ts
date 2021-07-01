19657b91fef1ae2896b19378b92dee8e40521d1d
Hextanium <hextanium.on.discord@gmail.com>
2021-06-30 23:09:55 -0400

    [fix] fix main.ts success message

--- source/main.ts
+++ source/main.ts
@@ -1,12 +1,16 @@
 import AntiClient from "./utilities/AntiClient";
 import {config as _envConfig} from "dotenv";
 
-_envConfig();
+(async () => {
+    _envConfig();
 
-const client: AntiClient = new AntiClient();
+    const client: AntiClient = new AntiClient();
 
-client.login(process.env.TOKEN).catch(err => {
-    throw new Error("There was an error while logging in.\n" + err);
-});
+    await client.login(process.env.TOKEN).catch(err => {
+        throw new Error("There was an error while logging in.\n" + err);
+    });
 
-console.info(`Logged in as ${client.user}`);
+    console.info(`Logged in as ${client.user.tag}`);
+})();
+
+

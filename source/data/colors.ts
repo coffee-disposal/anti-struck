12ff214509cd9cb3ed417c288348895ff19b019b
Hextanium <hextanium.on.discord@gmail.com>
2021-07-08 22:37:33 -0400

    [refactor] transform from JSON to TypeScript enum for ease of use

--- source/data/colors.ts
+++ source/data/colors.ts
@@ -1,6 +1,6 @@
-module.exports = {
-  info: "#4287f5",
-  warning: "#f5a442",
-  error: "#f54242",
-  success: "#42f560"
+export enum StatusColor {
+  INFO = "#4287f5",
+  WARNING = "#f5a442",
+  ERROR = "#f54242",
+  SUCCESS = "#42f560",
 }

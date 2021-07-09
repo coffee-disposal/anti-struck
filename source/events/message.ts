6666de2fbf90d75123054be13af199e4764d420f
Hextanium <hextanium.on.discord@gmail.com>
2021-07-08 22:41:26 -0400

    [add] command processing in the `message` event, basic errors

--- source/events/message.ts
+++ source/events/message.ts
@@ -1,12 +1,43 @@
-import {Event} from "../structures/Event";
 import AntiClient from "../utilities/AntiClient";
+import { StatusColor } from "../data/colors";
+import { Event } from "../structures/Event";
+import { Message, MessageEmbed } from "discord.js";
+import { table } from "quick.db";
 
-export class Message extends Event {
+export class MessageEvent extends Event {
     constructor() {
         super("message");
     }
 
-    execute(client: AntiClient, ...args) {
-        console.log(args[0].content, args[0].channel.id);
+    public async execute(client: AntiClient, ..._args): Promise<any> {
+        const message: Message = _args[0];
+        const settings: table = new (await import('quick.db')).table("settings"),
+              prefix = settings.get("prefix") || "sudo >>";
+
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+
+        const { developers } = (await import("../data/permissions"));
+        const [name, ...args] = message.content.slice(prefix.length).trim().split(/\s+/);
+
+        const command = client.commands.get(name);
+
+        if (command) {
+            if (command.developer && !developers.includes(message.author.id)) {
+                const embed: MessageEmbed = new MessageEmbed({
+                    title: "There was an error while executing your command!",
+                    description: "You do **not** have the `Developer` permission.",
+                    color: StatusColor.ERROR,
+                    footer: {
+                        text: message.author.tag,
+                        icon_url: message.author.displayAvatarURL() || `https://cdn.discordapp.com/embed/avatars/${Number(client.user.discriminator) % 5}.png`,
+                    }
+                });
+
+                return message.channel.send(embed);
+            }
+
+            command.run(client, message, args);
+        }
     }
 }

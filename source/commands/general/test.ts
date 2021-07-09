028da6010f79e8d47e8f23c907d89a489f758043
Hextanium <hextanium.on.discord@gmail.com>
2021-07-08 22:44:42 -0400

    [feat] remove export, make it a base command

--- source/commands/general/test.ts
+++ source/commands/general/test.ts
@@ -1,7 +1,7 @@
 import {Command} from "../../structures/Command";
 import {PermissionFlags} from "discord.js";
 
-export class Test extends Command {
+/* export */ class Test extends Command {
     constructor(name: string, category: string, aliases: string[] = [], description: string, permissions: PermissionFlags[], developer: boolean) {
         super(name, category, aliases, description, permissions, developer);
     }
